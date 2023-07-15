04eab0fe69968f2560f31a7be0f92df27cca68b9
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-07-14 19:58:42 -0600

    Cambios al 14/07/2023

--- app/src/main/java/com/example/tlenguajes2023/ActivityIngresar.java
+++ app/src/main/java/com/example/tlenguajes2023/ActivityIngresar.java
@@ -1,13 +1,24 @@
 package com.example.tlenguajes2023;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,9 +27,13 @@ import com.example.tlenguajes2023.configuracion.SQLiteConnection;
 
 public class ActivityIngresar extends AppCompatActivity {
 
+    public static final int peticion_acceso_camara = 101;
+    public static final int peticion_toma_fotografia = 102;
+    ImageView fotoview;
+
     EditText id, nombres, apellidos,  direccion, edad, correo;
     Spinner genero;
-    Button btningresar;
+    Button btningresar, btnfoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,7 +49,8 @@ public class ActivityIngresar extends AppCompatActivity {
         edad = (EditText) findViewById(R.id.txtedad);
         correo = (EditText) findViewById(R.id.txtcorreo);
         btningresar = (Button) findViewById(R.id.btningresar);
-
+        btnfoto = (Button) findViewById(R.id.btnfoto);
+        fotoview = (ImageView) findViewById(R.id.imageView);
         btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +58,60 @@ public class ActivityIngresar extends AppCompatActivity {
             }
         });
 
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Permisos();
+            }
+        });
+
+    }
+
+    private void Permisos()
+    {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA},peticion_acceso_camara);
+        }
+        else
+        {
+            TomarFoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            TomarFoto();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Permiso no otorgado",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void TomarFoto()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!= null)
+        {
+            startActivityForResult(intent,peticion_toma_fotografia);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == peticion_toma_fotografia)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imagen = (Bitmap) extras.get("data");
+            fotoview.setImageBitmap(imagen);
+        }
     }
 
     private void insertar_datos()
